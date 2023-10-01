89be4b08550bda14b9a2cfb0fd5cd356f5f71132
SeiOmori <sei.omori.22@gmail.com>
2023-10-01 15:34:20 +0900

    retry

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function($){
+    $('.slider').slick({
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        centerMode: true,
+        centerPadding: '20%',
+    });
+});
