f5856daaada72ea24a14da49b30a86c121eba153
SeiOmori <sei.omori.22@gmail.com>
2023-10-02 03:19:14 +0900

    add_humburger_menu

---
+++ js/about.js
@@ -0,0 +1,22 @@
+$(function($){
+    $('.slider').slick({
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        centerMode: true,
+        centerPadding: '20%',
+    });
+
+    $(document).on('click', '.ham_menu', function() {
+        console.log("click");
+        if($('.ham_menu').hasClass('active')) {
+            $('.ham_menu').removeClass('active');
+            $('.window-fixed').css('opacity', '0');
+            $('.window-fixed').css('transform', 'translateX(100%)');
+        } else {
+            $('.ham_menu').addClass('active');
+            $('.window-fixed').css('opacity', '1');
+            $('.window-fixed').css('transform', 'translateX(0%)');
+        }
+    });
+});
--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-$(function($){
-    $('.slider').slick({
-        arrows: false,
-        autoplay: true,
-        dots: true,
-        centerMode: true,
-        centerPadding: '20%',
-    });
+$(document).on('click', '.ham_menu', function() {
+    console.log("click");
+    if($('.ham_menu').hasClass('active')) {
+        $('.ham_menu').removeClass('active');
+        $('.window-fixed').css('opacity', '0');
+        $('.window-fixed').css('transform', 'translateX(100%)');
+    } else {
+        $('.ham_menu').addClass('active');
+        $('.window-fixed').css('opacity', '1');
+        $('.window-fixed').css('transform', 'translateX(0%)');
+    }
 });
+
+$(window).resize(function(){
+    if (window.matchMedia("(max-width: 1024px)").matches) {
+        $('.footer-img').attr('src', '/images/footer_sp.png');
+    }else{
+        // windowのサイズが1024px以上
+        $('.footer-img').attr('src', '/images/footer.png');
+    }
+});
+    
