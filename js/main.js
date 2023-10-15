e7d963ee1087e0ac9ba206ae2779e3f66ff76904
sei-lit <sei.omori.22@gmail.com>
2023-10-16 03:14:40 +0900

    responsive design in Top page has done.

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ $(window).resize(function(){
     if (window.matchMedia("(max-width: 600px)").matches) {
         $('.footer-img').attr('src', '/images/footer_sp.png');
     }else{
-        // windowのサイズが1024px以上
+        // windowのサイズが600px以上
         $('.footer-img').attr('src', '/images/footer.png');
     }
 });
