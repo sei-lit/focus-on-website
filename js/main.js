48e4520a074f76cfd2d09dbb1f891321cbc1a9f6
sei-lit <sei.omori.22@gmail.com>
2023-10-16 00:20:13 +0900

    responsive design in style.css

--- js/main.js
+++ js/main.js
@@ -9,14 +9,35 @@ $(document).on('click', '.ham_menu', function() {
         $('.window-fixed').css('opacity', '1');
         $('.window-fixed').css('transform', 'translateX(0%)');
     }
+
+    $('nav a[href^="#"]').click(function () {
+        var adjust = 0;
+        var speed = 400;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - adjust;
+        $('body,html').animate({
+          scrollTop: position
+        }, speed, 'swing');
+        return false;
+      });
 });
 
-// $(window).resize(function(){
-//     if (window.matchMedia("(max-width: 1024px)").matches) {
-//         $('.footer-img').attr('src', '/images/footer_sp.png');
-//     }else{
-//         // windowのサイズが1024px以上
-//         $('.footer-img').attr('src', '/images/footer.png');
-//     }
-// });
+$(window).on("load", function(){
+  if (window.matchMedia("(max-width: 600px)").matches) {
+      $('.footer-img').attr('src', '/images/footer_sp.png');
+  }else{
+      // windowのサイズが1024px以上
+      $('.footer-img').attr('src', '/images/footer.png');
+  }
+});
+
+$(window).resize(function(){
+    if (window.matchMedia("(max-width: 600px)").matches) {
+        $('.footer-img').attr('src', '/images/footer_sp.png');
+    }else{
+        // windowのサイズが1024px以上
+        $('.footer-img').attr('src', '/images/footer.png');
+    }
+});
     
